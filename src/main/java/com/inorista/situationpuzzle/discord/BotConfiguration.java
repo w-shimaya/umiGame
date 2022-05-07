066ad29312c486d232b3208dfdf063d84046732e
w-shimaya <w.shimaya@gmail.com>
2022-05-07 21:45:33 +0900

    Disable discord4j in testing

--- src/main/java/com/inorista/situationpuzzle/discord/BotConfiguration.java
+++ src/main/java/com/inorista/situationpuzzle/discord/BotConfiguration.java
@@ -12,9 +12,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import reactor.core.publisher.Mono;
 
 @Configuration
+@Profile({"prod"})
 public class BotConfiguration {
     @Value("${discord.token}")
     private String token;
--- src/test/java/com/inorista/situationpuzzle/repository/GameRepositoryImplTests.java
+++ src/test/java/com/inorista/situationpuzzle/repository/GameRepositoryImplTests.java
@@ -23,12 +23,13 @@ import java.util.Optional;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
 @SpringBootTest
+@ActiveProfiles({"test"})
 @TestExecutionListeners({
         DependencyInjectionTestExecutionListener.class,
         DbUnitTestExecutionListener.class
